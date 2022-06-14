446f0e1a73b879e943beb973420f7961489c0502
Yimin Zheng <yiminzheng29@gmail.com>
2022-06-14 17:57:57 +0800

    test commit

--- workshop/src/main/java/vttp/day7/App.java
+++ workshop/src/main/java/vttp/day7/App.java
@@ -1,13 +1,11 @@
 package vttp.day7;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+
+public class App {
+    public static void main( String[] args ) throws IOException {
+        BooksDatabase db = new BooksDatabase(args[0]);
+        Session sess = new Session(db);
+        sess.run();
     }
 }
